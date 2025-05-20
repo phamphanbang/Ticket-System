7eeab07125e86aab9e9d0540e43a0fe1de013ec5
phamphanbang <phamphanbang@gmail.com>
2025-05-20 15:09:19 +0700

    catch and render exception

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,18 +1,32 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Validation\UnauthorizedException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    ->withMiddleware(function (Middleware $middleware) {})
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(
+            function (Exception $e, Request $request) {
+                if ($e instanceof AuthenticationException) {
+                    return response()->error(
+                        message: __('messages.unauthenticated'),
+                        errors: null,
+                        code: Response::HTTP_UNAUTHORIZED
+                    );
+                }
+            }
+        );
     })->create();
---
+++ src/lang/en/messages.php
@@ -0,0 +1,4 @@
+<?php 
+return [
+  'unauthenticated' => 'Invalid or expired token. Please authenticate again.'
+];
