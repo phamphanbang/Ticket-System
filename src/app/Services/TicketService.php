1ff48e7bd935dcde124d70175642896a0279d429
phamphanbang <phamphanbang@gmail.com>
2025-05-22 12:01:45 +0700

    Create api resolve ticket

--- src/app/Services/TicketService.php
+++ src/app/Services/TicketService.php
@@ -8,6 +8,7 @@ use App\Exceptions\InvalidTicketAssignmentException;
 use App\Mail\ClientAdminAssignStaff;
 use App\Mail\ClientTicketCreated;
 use App\Mail\ClientTicketIsConfirmed;
+use App\Mail\ClientTicketIsResolved;
 use App\Mail\StaffAssignedToNewTicket;
 use App\Models\Ticket;
 use App\Validators\TicketValidator;
@@ -59,6 +60,7 @@ class TicketService
   public function adminAssignTicket($data, $id)
   {
     $ticket = Ticket::where('id', $id)->first();
+
     TicketValidator::checkTicketExists($ticket);
     TicketValidator::checkTicketStatus($ticket, TicketStatus::New->value);
     TicketValidator::checkTicketNotAssignedToTheSameStaff($ticket, $data['assign_to']);
@@ -88,6 +90,22 @@ class TicketService
     $ticket->update($data);
 
     Mail::to($ticket->client_email)->queue(new ClientTicketIsConfirmed($ticket));
+
+    return $ticket;
+  }
+
+  public function staffResolveTicket($data, $id)
+  {
+    $user = auth()->user();
+    $ticket = Ticket::where('id', $id)->first();
+
+    TicketValidator::checkTicketExists($ticket);
+    TicketValidator::checkTicketStatus($ticket, TicketStatus::InProgress->value);
+    TicketValidator::checkStaffIsAssignedToTicket($ticket, $user->id);
+
+    $ticket->update($data);
+
+    Mail::to($ticket->client_email)->queue(new ClientTicketIsResolved($ticket));
 
     return $ticket;
   }
