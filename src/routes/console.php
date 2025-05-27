37962ee97a8a5605862dc49cee6f2bf6e193e860
phamphanbang <phamphanbang@gmail.com>
2025-05-27 09:52:19 +0700

    update cronjob and log

--- src/routes/console.php
+++ src/routes/console.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote');
 Artisan::command('ticket:fetch-emails', function () {
-
-})->everyFiveMinutes();
+  Log::info('Fetching emails for tickets at ' . now());
+})->everyFiveMinutes()->withoutOverlapping();
 // Schedule::command('ticket:fetch-emails')->everyFiveMinutes();
