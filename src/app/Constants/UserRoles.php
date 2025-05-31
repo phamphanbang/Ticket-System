1f6ce9e9998b7c613f473a0d46fccf8e1f7631ac
phamphanbang <phamphanbang@gmail.com>
2025-05-31 21:26:07 +0700

    feature(models): update user model

--- src/app/Constants/UserRoles.php
+++ src/app/Constants/UserRoles.php
@@ -6,12 +6,16 @@ enum UserRoles: string
 {
     case ADMIN = 'admin';
     case STAFF = 'staff';
+    case LEADER = 'leader';
+    case SUPPORTER = 'supporter';
 
     public function label(): string
     {
         return match ($this) {
             self::ADMIN => 'admin',
-            self::STAFF => 'staff'
+            self::STAFF => 'staff',
+            self::LEADER => 'leader',
+            self::SUPPORTER => 'supporter'
         };
     }
 }
