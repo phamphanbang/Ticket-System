daf0ac25679c6a32b0aa954849d55ff490818a85
phamphanbang <phamphanbang@gmail.com>
2025-06-02 16:29:05 +0700

    feat: update api and migration

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,28 +14,43 @@ Route::middleware('auth:sanctum')->post('auth/logout', [AuthController::class, '
 Route::middleware(['auth:sanctum','api'])->group(function () {
   Route::apiResource('users', UserController::class);
 
-  Route::apiResource('tickets', TicketController::class);
-
-  Route::put('tickets/{id}/awaiting-client-approval', [TicketController::class, 'awaitingForClientApproval']);
-  Route::put('tickets/{id}/client-approve', [TicketController::class, 'clientApprove']);
-
-  // Route::apiResource('tasks', TaskController::class);
-  Route::get('tickets/{id}/tasks', [TaskController::class, 'index']);
-  Route::get('tasks/{id}', [TaskController::class, 'show']);
-  Route::post('tickets/{id}/tasks', [TaskController::class, 'store']);
-  Route::put('tasks/{id}', [TaskController::class, 'update']);
-  Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
-  Route::post('tasks/{id}/assign-staff', [TaskController::class, 'assignStaff']);
-  Route::post('tasks/{id}/ready-to-review', [TaskController::class, 'readyToReview']);
-  Route::post('tasks/{id}/notify-leader-for-review', [TaskController::class, 'notifyLeaderForReview']);
-  Route::post('tasks/{id}/mark-estimate-needs-revision', [TaskController::class, 'needsRevision']);
-  Route::post('tasks/{id}/mark-estimate-approved', [TaskController::class, 'estimateApproved']);
-  Route::post('tasks/{id}/start-execution', [TaskController::class, 'startExecution']);
-  Route::post('tasks/{id}/block', [TaskController::class, 'blockTask']);
+  Route::prefix('tickets')->group(function () {
+    Route::get('/', [TicketController::class, 'index']);
+    Route::post('/', [TicketController::class, 'store']);
+    Route::get('/{id}', [TicketController::class, 'show']);
+    Route::put('/{id}', [TicketController::class, 'update']);
+    Route::delete('/{id}', [TicketController::class, 'destroy']);
+    
+    // Ticket status transitions
+    Route::post('/{id}/status/awaiting-client-approval', [TicketController::class, 'awaitingForClientApproval']);
+    Route::post('/{id}/status/client-approve', [TicketController::class, 'clientApprove']);
+    
+    // Nested task routes
+    Route::prefix('/{ticketId}/tasks')->group(function () {
+        Route::get('/', [TaskController::class, 'index']);
+        Route::post('/', [TaskController::class, 'store']);
+    });
+});
 
   Route::get('/tickets/{id}/participants', [TicketParticipantController::class, 'index']);
   Route::post('/tickets/{id}/participants', [TicketParticipantController::class, 'store']);
   Route::delete('/participants/{id}', [TicketParticipantController::class, 'destroy']);
 
+// Task routes
+Route::prefix('tasks')->group(function () {
+    Route::get('/{id}', [TaskController::class, 'show']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
+    
+    // Task status transitions
+    Route::post('/{id}/status/assign', [TaskController::class, 'assignStaff']);
+    Route::post('/{id}/status/ready-for-review', [TaskController::class, 'readyToReview']);
+    Route::post('/{id}/status/needs-revision', [TaskController::class, 'needsRevision']);
+    Route::post('/{id}/status/estimate-approved', [TaskController::class, 'estimateApproved']);
+    Route::post('/{id}/status/start-execution', [TaskController::class, 'startExecution']);
+    Route::post('/{id}/status/block', [TaskController::class, 'blockTask']);
+    Route::post('/{id}/status/change-request', [TaskController::class, 'changeRequest']);
+    Route::post('/{id}/status/execution-ready', [TaskController::class, 'executionReadyToReview']);
+});
 });
 
