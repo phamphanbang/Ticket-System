6b2f517b7a88e149001ef3faabf8e97206f4c302
phamphanbang <phamphanbang@gmail.com>
2025-06-03 09:40:42 +0700

    fix: refactor and add delete multiple participant

--- src/routes/api.php
+++ src/routes/api.php
@@ -22,11 +22,11 @@ Route::middleware(['auth:sanctum','api'])->group(function () {
     Route::delete('/{id}', [TicketController::class, 'destroy']);
     
     // Ticket status transitions
-    Route::post('/{id}/status/awaiting-client-approval', [TicketController::class, 'awaitingForClientApproval']);
-    Route::post('/{id}/status/client-approve', [TicketController::class, 'clientApprove']);
+    Route::post('/{id}/awaiting-client-approval', [TicketController::class, 'awaitingForClientApproval']);
+    Route::post('/{id}/client-approve', [TicketController::class, 'clientApprove']);
     
     // Nested task routes
-    Route::prefix('/{ticketId}/tasks')->group(function () {
+    Route::prefix('/{id}/tasks')->group(function () {
         Route::get('/', [TaskController::class, 'index']);
         Route::post('/', [TaskController::class, 'store']);
     });
@@ -35,6 +35,7 @@ Route::middleware(['auth:sanctum','api'])->group(function () {
   Route::get('/tickets/{id}/participants', [TicketParticipantController::class, 'index']);
   Route::post('/tickets/{id}/participants', [TicketParticipantController::class, 'store']);
   Route::delete('/participants/{id}', [TicketParticipantController::class, 'destroy']);
+  Route::post('/tickets/{id}/participants/multiple', [TicketParticipantController::class, 'destroyMultiple']);
 
 // Task routes
 Route::prefix('tasks')->group(function () {
@@ -43,14 +44,14 @@ Route::prefix('tasks')->group(function () {
     Route::delete('/{id}', [TaskController::class, 'destroy']);
     
     // Task status transitions
-    Route::post('/{id}/status/assign', [TaskController::class, 'assignStaff']);
-    Route::post('/{id}/status/ready-for-review', [TaskController::class, 'readyToReview']);
-    Route::post('/{id}/status/needs-revision', [TaskController::class, 'needsRevision']);
-    Route::post('/{id}/status/estimate-approved', [TaskController::class, 'estimateApproved']);
-    Route::post('/{id}/status/start-execution', [TaskController::class, 'startExecution']);
-    Route::post('/{id}/status/block', [TaskController::class, 'blockTask']);
-    Route::post('/{id}/status/change-request', [TaskController::class, 'changeRequest']);
-    Route::post('/{id}/status/execution-ready', [TaskController::class, 'executionReadyToReview']);
+    Route::post('/{id}/assign', [TaskController::class, 'assignStaff']);
+    Route::post('/{id}/ready-for-review', [TaskController::class, 'readyToReview']);
+    Route::post('/{id}/needs-revision', [TaskController::class, 'needsRevision']);
+    Route::post('/{id}/estimate-approved', [TaskController::class, 'estimateApproved']);
+    Route::post('/{id}/start-execution', [TaskController::class, 'startExecution']);
+    Route::post('/{id}/block', [TaskController::class, 'blockTask']);
+    Route::post('/{id}/change-request', [TaskController::class, 'changeRequest']);
+    Route::post('/{id}/execution-ready', [TaskController::class, 'executionReadyToReview']);
 });
 });
 
