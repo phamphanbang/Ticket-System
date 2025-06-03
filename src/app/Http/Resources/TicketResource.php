9fdd3280339b8486081d2f99b9944f7d5de7c297
phamphanbang <phamphanbang@gmail.com>
2025-06-03 08:43:05 +0700

    fix: adjust ticket resource

--- src/app/Http/Resources/TicketResource.php
+++ src/app/Http/Resources/TicketResource.php
@@ -22,7 +22,9 @@ class TicketResource extends JsonResource
             'client_name' => $this->client?->name,
             'internal_status' => $this->internal_status,
             'external_status' => $this->external_status,
-            'assigned_to' => $this->participants->where('role_in_ticket', 'leader')->first()?->user?->name,
+            'assigned_to' => $this->participants->where('role_in_ticket', 'leader')->map(function ($participant) {
+                return $participant->user->name;
+            })->values()->all(),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
