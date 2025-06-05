c88bcb005cc5ee6862e74d84e8c40c9dca67e5a9
phamphanbang <phamphanbang@gmail.com>
2025-06-05 10:39:12 +0700

    feat: update api

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,19 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UserController;
 
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('auth/logout', [AuthController::class, 'logout']);
 
-Route::middleware(['auth:sanctum','api'])->group(function () {
+Route::middleware(['auth:sanctum', 'api'])->group(function () {
   Route::apiResource('users', UserController::class);
   Route::apiResource('tickets', TicketController::class);
   Route::prefix('tickets')->group(function () {
-    
     Route::get('/{id}/logs', [TicketController::class, 'getLogs']);
+    Route::get('/{id}/comments', [CommentController::class, 'index']);
+    Route::post('/{id}/comments', [CommentController::class, 'store']);
+    Route::get('/{id}/comments/{commentId}', [CommentController::class, 'show']);
+  });
+  Route::put('/comments/{commentId}', [CommentController::class, 'update']);
+  Route::delete('/comments/{commentId}', [CommentController::class, 'destroy']);
 });
-
-});
-
