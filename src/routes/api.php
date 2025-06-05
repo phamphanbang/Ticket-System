b5646f055a946be47dd361852bdf74fce08d4530
phamphanbang <phamphanbang@gmail.com>
2025-06-05 09:43:32 +0700

    feat: update api

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\TicketParticipantController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UserController;
 
@@ -13,45 +10,11 @@ Route::middleware('auth:sanctum')->post('auth/logout', [AuthController::class, '
 
 Route::middleware(['auth:sanctum','api'])->group(function () {
   Route::apiResource('users', UserController::class);
-
+  Route::apiResource('tickets', TicketController::class);
   Route::prefix('tickets')->group(function () {
-    Route::get('/', [TicketController::class, 'index']);
-    Route::post('/', [TicketController::class, 'store']);
-    Route::get('/{id}', [TicketController::class, 'show']);
-    Route::put('/{id}', [TicketController::class, 'update']);
-    Route::delete('/{id}', [TicketController::class, 'destroy']);
     
-    Route::post('/{id}/execute', [TicketController::class, 'executeTicket']);
-    Route::post('/{id}/close', [TicketController::class, 'checkAndCloseTicket']);
-    Route::get('/{id}/audit-logs', [TicketController::class, 'getTicketAuditLogs']);
-    Route::prefix('/{id}/tasks')->group(function () {
-        Route::get('/', [TaskController::class, 'index']);
-        Route::post('/', [TaskController::class, 'store']);
-    });
+    Route::get('/{id}/logs', [TicketController::class, 'getLogs']);
 });
 
-  Route::get('/tickets/{id}/participants', [TicketParticipantController::class, 'index']);
-  Route::post('/tickets/{id}/participants', [TicketParticipantController::class, 'store']);
-  Route::delete('/participants/{id}', [TicketParticipantController::class, 'destroy']);
-  Route::post('/tickets/{id}/participants/multiple', [TicketParticipantController::class, 'destroyMultiple']);
-
-// Task routes
-Route::prefix('tasks')->group(function () {
-    Route::get('/{id}', [TaskController::class, 'show']);
-    Route::put('/{id}', [TaskController::class, 'update']);
-    Route::delete('/{id}', [TaskController::class, 'destroy']);
-    Route::get('/{id}/audit-logs', [TaskController::class, 'getTaskAuditLogs']);
-    
-    // Task status transitions
-    Route::post('/{id}/assign', [TaskController::class, 'assignStaff']);
-    Route::post('/{id}/ready-for-review', [TaskController::class, 'readyToReview']);
-    Route::post('/{id}/needs-revision', [TaskController::class, 'needsRevision']);
-    Route::post('/{id}/estimate-approved', [TaskController::class, 'estimateApproved']);
-    Route::post('/{id}/start-execution', [TaskController::class, 'startExecution']);
-    Route::post('/{id}/block', [TaskController::class, 'blockTask']);
-    Route::post('/{id}/change-request', [TaskController::class, 'changeRequest']);
-    Route::post('/{id}/execution-ready', [TaskController::class, 'executionReadyToReview']);
-    Route::post('/{id}/execution-complete', [TaskController::class, 'completeExecution']);
-});
 });
 
