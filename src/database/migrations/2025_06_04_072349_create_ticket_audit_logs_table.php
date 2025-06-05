217e3ae98e917633b56ab8964e84a6c038c6db1e
phamphanbang <phamphanbang@gmail.com>
2025-06-05 09:42:27 +0700

    ref: update audit migration

--- src/database/migrations/2025_06_04_072349_create_ticket_audit_logs_table.php
+++ src/database/migrations/2025_06_04_072349_create_ticket_audit_logs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('ticket_audit_logs', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('ticket_id')->constrained('tickets');
+            $table->foreignUuid('ticket_id')->constrained('tickets')->onDelete('cascade');
             $table->string('action');
             $table->string('status');
             $table->string('to_status')->nullable();
