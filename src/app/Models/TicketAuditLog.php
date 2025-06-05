f74f8174b16bd404f3dfa786051c64af157d4ef4
phamphanbang <phamphanbang@gmail.com>
2025-06-05 10:38:57 +0700

    feat: add softdelete in audit log

--- src/app/Models/TicketAuditLog.php
+++ src/app/Models/TicketAuditLog.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class TicketAuditLog extends Model
 {
-    use HasUuids;
+    use HasUuids, SoftDeletes;
 
     public $timestamps = false;
 
