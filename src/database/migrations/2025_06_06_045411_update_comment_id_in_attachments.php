b1bde8f2fe3e56e197b5d9e6e1dcd02f54010621
phamphanbang <phamphanbang@gmail.com>
2025-06-06 13:55:06 +0700

    feat: add ticket_id in attachment

---
+++ src/database/migrations/2025_06_06_045411_update_comment_id_in_attachments.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->dropForeign(['comment_id']);
+            $table->dropColumn('comment_id');
+            $table->foreignUuid('comment_id')->nullable()->references('id')->on('ticket_comments')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attachments', function (Blueprint $table) {
+            //
+        });
+    }
+};
