36a8fa7c396ffb7837673e4903776f01e7c77b39
phamphanbang <phamphanbang@gmail.com>
2025-06-06 14:26:11 +0700

    ref: removed log from comment controller

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -27,7 +27,6 @@ class CommentController extends Controller
 
     public function store(CreateCommentRequest $request, $ticket_id)
     {
-        dd($request);
         $validated = $request->validated();
 
         $user = Auth::user();
