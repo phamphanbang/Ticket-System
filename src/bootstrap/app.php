61bc5316e2744a16eda1c111fe79d5d9be2be1fb
phamphanbang <phamphanbang@gmail.com>
2025-06-26 09:57:43 +0700

    ref: refactor command

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -38,7 +38,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withCommands([
-        // \App\Console\Commands\FetchClientMails::class,
         \App\Console\Commands\SyncSlackUsers::class,
     ])
     ->withExceptions(function (Exceptions $exceptions) {
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,39 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Services\GmailService;
-use Illuminate\Http\Request;
-use Google\Client;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/oauth2callback', function (Request $request) {
-    $code = $request->get('code');
-
-    if (!$code) {
-        return response('Authorization code not provided.', 400);
-    }
-
-    $client = new Client();
-    $client->setAuthConfig(storage_path('app/google/credentials.json'));
-    $client->setRedirectUri(url('/oauth2callback')); // Ensure this matches the one in Google Console
-    $client->setScopes(['https://www.googleapis.com/auth/gmail.readonly']);
-    $client->setAccessType('offline');
-
-    try {
-        $accessToken = $client->fetchAccessTokenWithAuthCode($code);
-
-        if (isset($accessToken['error'])) {
-            return response('Error fetching token: ' . $accessToken['error_description'], 400);
-        }
-
-        // Save token to storage
-        // file_put_contents('../storage/app/google/token.json', json_encode($accessToken));
-
-        return response($accessToken);
-    } catch (\Exception $e) {
-        return response('Exception: ' . $e->getMessage(), 500);
-    }
 });
