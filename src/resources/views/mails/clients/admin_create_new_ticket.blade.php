9266a8a87ab1f02ed6ca6e5be93207edee7494a9
phamphanbang <phamphanbang@gmail.com>
2025-06-26 14:51:53 +0700

    feat: update mail content

--- src/resources/views/mails/clients/admin_create_new_ticket.blade.php
+++ src/resources/views/mails/clients/admin_create_new_ticket.blade.php
@@ -4,6 +4,8 @@
 
 <p>Thank you for reaching out to <strong>{{ config('app.name') }} Support</strong>. This is to confirm that we have received your inquiry and a support ticket has been successfully created in our system.</p>
 
+<p><strong>Ticket ID:</strong> {{ $ticket->id }}</p>
+
 <p>Our support team is currently reviewing your request and will get back to you as soon as possible. If you have any additional information to share, please feel free to reply to this email. Your response will be automatically added to the ticket thread for our team to review.</p>
 
 <p>We appreciate your patience and will ensure your issue is addressed promptly.</p>
